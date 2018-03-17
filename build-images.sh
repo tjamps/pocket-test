cf977b22b605d542b035bb879bbc15b05c05a77c
Rémi T'JAMPENS <remi.tjampens@gmail.com>
2018-03-17 20:10:13 +0100

    Do not use cache when building images

--- build-images.sh
+++ build-images.sh
@@ -4,7 +4,7 @@ TAGS="7.1 7.2"
 
 for tag in ${TAGS};
 do
-    docker build -t pocket-test:${tag} -t tjamps/pocket-test:${tag} -f ${tag}/Dockerfile .
+    docker build --no-cache -t pocket-test:${tag} -t tjamps/pocket-test:${tag} -f ${tag}/Dockerfile .
     if [ $? -ne 0 ]; then
         exit 1
     fi
